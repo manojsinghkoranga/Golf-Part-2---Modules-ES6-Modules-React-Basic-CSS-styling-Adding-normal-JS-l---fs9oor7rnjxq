5d526c82570c43ac7b8806a8cafb814e983e9c0c
manoj singh <117524976+manojsinghkoranga@users.noreply.github.com>
2023-01-19 18:47:27 +0000

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Component, useState } from "react";
+import React, { Component, useEffect, useState } from "react";
 import "../styles/App.css";
 
 const App = () => {
@@ -9,12 +9,53 @@ const App = () => {
     left: "0px",
     top: "0px",
   });
-  const reset = () => {};
-  const renderChoice = () => {};
+  const reset = () => {
+    setRenderBall(false);
+    setX(0);
+    setY(0);
+  };
+  const renderChoice = () => {
+    if(renderBall) {
+      return ( 
+        <>
+          <div className="ball" style={{left: `${x}px`, top: `${y}px`}}></div>
+        </>
+      )
+    }
+
+    return (
+      <button onClick={() => setRenderBall(true)} className="start">
+        Start
+      </button>
+    )
+  };
+
+  useEffect(() => {
+    const handler = (event) => {
+      if(event.key === "ArrowRight"){
+        setX(x+5);
+      }
+      if(event.key === "ArrowUp"){
+        setY(y-5);
+      }
+      if(event.key === "ArrowLeft"){
+        setX(x-5);
+      }
+      if(event.key === "ArrowDown"){
+        setY(y+5);
+      }
+    }
+
+    window.addEventListener("keydown", handler);
+
+    return () => {
+      window.removeEventListener("keydown", handler);
+    }
+  }, [x, y])
 
   return (
     <div className="playground">
-      <button onClick={reset} className="reset">
+      <button  className="reset" onClick={reset}>
         Reset
       </button>
       {renderChoice()}
